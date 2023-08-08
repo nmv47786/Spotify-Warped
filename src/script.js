75ac29a67f49a76905d6736429bfd7b9a85b248e
nmv47786 <nmv47786@uga.edu>
2023-08-08 09:38:40 -0400

    debug

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 //import { SpotifyClientId, TMClientId } from './apiKeys.js';
 
-const SpotifyClientId = SPOTIFY_CLIENT_ID;
-const TMClientId = TM_CLIENT_ID;
+const SpotifyClientId = "${SPOTIFY_CLIENT_ID}";
+const TMClientId = "${TM_CLIENT_ID}";
 console.log(SpotifyClientId);
 console.log(TMClientId);
 const params = new URLSearchParams(window.location.search);
