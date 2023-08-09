b2ec0df62d1e206039a8f748a18c19ba27159fc8
nmv47786 <nmv47786@uga.edu>
2023-08-09 15:05:24 -0400

    typo bruh

--- src/script.js
+++ src/script.js
@@ -39,7 +39,7 @@ export async function redirectToAuthCodeFlow(SpotifyClientId) {
     params.append("client_id", SpotifyClientId);
     params.append("response_type", "code");
     //params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("redirect_uri", "https://spotify-wraped.netlify.app/callback");
+    params.append("redirect_uri", "https://spotify-warped.netlify.app/callback");
     params.append("scope", "user-read-private user-read-email user-top-read user-follow-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -75,7 +75,7 @@ export async function getAccessToken(SpotifyClientId, code) {
     params.append("grant_type", "authorization_code");
     params.append("code", code);
     //params.append("redirect_uri", "http://localhost:5173/callback");
-    params.append("redirect_uri", "https://spotify-wraped.netlify.app/callback");
+    params.append("redirect_uri", "https://spotify-warped.netlify.app/callback");
     params.append("code_verifier", verifier);
     const body = new URLSearchParams(params).toString();
     console.log("body",body);
