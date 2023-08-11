8a46c9e520d85cb8c0017f4c11368617baf34d04
nmv47786 <nmv47786@uga.edu>
2023-08-11 13:52:05 -0400

    ee

--- src/script.js
+++ src/script.js
@@ -258,15 +258,12 @@ async function getAudioFeatures(token, songs) {
 // Top genres
 async function getTopGenres(token) {
     const result = await fetchWebApi('v1/me/top/tracks?offset=0&limit=50', 'GET', undefined, token);
-    const result2 = await fetchWebApi('v1/me/top/tracks?offset=50&limit=50', 'GET', undefined, token);
     const tracks = result.items;
-    const tracks2 = result2.items;
-    const allTracks = [...tracks, ...tracks2];
     const allGenres = {};
 
-    console.log("allTracks:", allTracks);
+    console.log("allTracks:", tracks);
 
-    allTracks.forEach(track => {
+    tracks.forEach(track => {
         if (track.artists && Array.isArray(track.artists)) {
             track.artists.forEach(artist => {
                 if (artist.genres && Array.isArray(artist.genres)) {
